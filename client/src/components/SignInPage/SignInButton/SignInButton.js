543feb2d926bdb780a0f5a730bd291a762e37439
koneone2002 <katie@imagings-MacBook-Pro.local>
2018-10-16 15:54:28 -0700

    sign In Page added

---
+++ client/src/components/SignInPage/SignInButton/SignInButton.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Foundation from 'react-foundation';
+
+import "./SignInButton.css"
+
+// This Col component lets us size bootstrap columns with less syntax
+// e.g. <Col size="md-12"> instead of <div className="col-md-12">
+const SignInButton = ({ size, children }) => (
+  
+  <button className="next-button center">Next</button>
+
+
+);
+
+
+
+export default SignInButton;
