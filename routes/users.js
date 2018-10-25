5e2b8fecfc7b60ba1d236c45a1d65115464d89b7
chriskelsey <chriskelsey.net@gmail.com>
2018-10-24 22:00:19 -0700

    Cleaned up and tested api routes

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const passport = require('passport');
 const router = require('express').Router();
-const auth = require('../auth');
+const auth = require('./auth');
 const Users = mongoose.model('Users');
 
 //POST new user route (optional, everyone has access)
