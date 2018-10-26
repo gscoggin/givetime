30e30c758ad68fa13c25bd8862393f8f2ed4095e
Tam Hong <tamhong.sd@gmail.com>
2018-10-26 16:42:42 -0700

    charity sign up to database

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,9 @@ import axios from "axios";
 export default {
     newVolunteer: function(volunteerData) {
         return axios.post("/api/register", volunteerData)
+    },
+
+    newCharity: function(charityData) {
+        return axios.post("/api/charitysignup", charityData)
     }
 }
--- models/charity.js
+++ models/charity.js
@@ -2,12 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const charitySchema = new Schema({
+    username: { type: String, required: true, trim: true },
     name: { type: String, required: true, trim: true },
     password: { type: String, required: true, trim: true },
-    irsStatus: { type: String, required: true, trim: true },
+    category: { type: String, required: true, trim: true },
     email: { type: String, required: true, trim: true },
-    synopsis: { type: String, required: true, trim: true },
-
+    phoneNumber: { type: String, required: true, trim: true },
+    irsStatus: { type: String, required: true, trim: true },
+    mission: { type: String, required: false, trim: true },
 });
 
 const Charity = mongoose.model("Charity", charitySchema);
