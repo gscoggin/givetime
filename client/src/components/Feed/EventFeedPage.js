397ae355ff8277f847e18318815ac36956ea6cea
koneone2002 <katie@imagings-MacBook-Pro.local>
2018-10-26 17:42:51 -0700

    eventfeed stuff

--- client/src/components/Feed/EventFeedPage.js
+++ client/src/components/Feed/EventFeedPage.js
@@ -1,26 +1,56 @@
-import React from "react";
+import React, {Component} from "react";
 import NavSideBar from "../Nav"
 import FeedListItem from "./FeedListItem.js";
 import "./EventFeedPage.css";
 import MyEvent from '../MyEvents/MyEvent';
 import Footer from '../Nav/Footer';
 
-const EventFeedPage = ({ size, children }) => (
-    <div>
-        <div>
-        <NavSideBar />
+
+
+class EventFeedPage extends Component {
+    state = {
+        show: true
+        
+    }
+
+    showToggle =() => {
+        if(this.state.show) {
+            this.setState({show:false})
+        } else {
+            this.setState({show:true})
+        }
+        
+    
+    }
+    render () {
+        return (
+            
+            <div className="row">
+
+        <div className="small-12 medium-12 columns">
+        <NavSideBar 
+            toggle={this.showToggle}/>
         </div>
 
-        <div className = "content">
+        <div className = "content col md-10">
             <div className= "blank "></div>
             <h2>Event Feed</h2>
             <FeedListItem />
         </div>
         <div>
-         <MyEvent/>
+         <MyEvent
+            state={this.state.show}
+            
+            />
         </div>
         <Footer /> 
     </div>
-);
+
+        );
+    }
+
+}
+    
+
 
 export default EventFeedPage;
